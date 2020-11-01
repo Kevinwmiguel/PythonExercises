295f0ec76e6ead086937f437c5a4f52d51411f85
Kevinwmiguel <kevincursos@outlook.com>
2020-11-01 22:53:10 +0000

    added Exercises 63 and 64

---
+++ Ex63.py
@@ -0,0 +1,48 @@
+"""
+Ex 63 - create a program that reads the name and
+price of various products. the program should ask
+if the user will continue. At the end, show:
+A) what is the total amount spent on the purchase
+B) how many products cost more than 1000
+C) What is the cheapest product name
+"""
+# ----- Function -----
+def line():
+    print('=' * 20)
+
+
+def header():
+    line()
+    print('CHEAPEST STORE'.center(20))
+    line()
+
+# ----- Header -----
+header()
+
+# ----- var zone -----
+total_price = expensive = current_price = count = 0
+p_name = ''
+
+
+# ----- Loop Zone ----- 
+while True:
+    product_name = str(input('Product name: ')).strip()
+    price = float(input('Price: '))
+    total_price = total_price + price
+    if price > 1000:
+        expensive = expensive + 1
+    if count == 0:
+        current_price = price
+    if price <= current_price:
+        current_price = price
+        p_name = product_name
+    count = count + 1
+    answer = str(input('Do you want continue? [Y/N]: ')).upper()
+    if answer == 'N':
+        break
+line()
+print(f'The total was RS{total_price}')
+print(f'Has {expensive} products that cost more than 1000,00')
+print(f'The cheapest product was {p_name} with the price of {current_price}')
+line()
+input('Enter to exit')
---
+++ Ex64.py
@@ -0,0 +1,73 @@
+"""
+EX 64 - Create a program that simulates the functioning of an ATM.
+at the beginning ask the user what will be the amount to be withdrawn (whole number)
+ and the program will inform how many bills of each amount will be delivered
+ OBS: consider that the cashier has bills of 50 20 10 and 1
+
+"""
+
+# ----- import zone -----
+from emoji import emojize           # using a emojis in program
+
+# ----- Function zone -----
+
+def line():
+    print('==' * 20)
+
+
+def header():
+    line()
+    print('BANCO CEV'.center(40))
+    line()
+
+
+# ----- Begin -----
+header()
+
+# ----- Var Zone -----
+v = int(input('How much do you want withdraw?: '))
+c = v
+c1 = c5 = c10 = c20 = c50 = c100 = c200 = 0
+
+# ----- Loop Zone -----
+while c != 0:                                                               # Check if the current money is higher 
+    if c >= 200:                                                            #than the bill and if is add a bill with 
+        c200 = c200 + 1                                                     #the right value. And discont this money to the money
+        c = c - 200
+    elif c >= 100:
+        c100 = c100 + 1
+        c = c - 100
+    elif c >= 50:
+        c50 = c50 + 1
+        c = c - 50
+    elif c >= 20:
+        c20 = c20 + 1
+        c = c - 20
+    elif c >= 10:
+        c10 = c10 + 1
+        c = c - 10
+    elif c >= 5:
+        c5 = c5 + 1
+        c = c - 5
+    else:
+        c1 = c1 + 1
+        c = c - 1
+# ----- Conditional Zone -----
+if c200 >= 1:                                                                                                # check if the note exist and if True put 
+    print(emojize(f'\033[30;43mtotal of {c200} bill of R$200 \033[m :wolf:', use_aliases='True'))            #print the number of notes the user will    
+if c100 >= 1:                                                                                                # get
+    print(emojize(f'\033[36mtotal of {c100} bill of 100\033[m :fish:', use_aliases='True'))                  
+if c50 >= 1:
+    print(emojize(f'\033[30;45mtotal of {c50} bill of 50\033[m :tiger:', use_aliases='True'))
+if c20 >= 1:
+    print(emojize(f'\033[34mtotal of {c20} bill of 20\033[m :monkey_face:', use_aliases='True'))
+if c10 >= 1:
+    print(emojize(f'\033[32mtotal of {c10} bill of 10\033[m :snake:', use_aliases='True'))
+if c5 >= 1:
+    print(emojize(f'\033[30;41mtotal of {c5} bill of 5\033[m :bear:', use_aliases='True'))
+if c1 >= 1:
+    print(emojize(f'\033[30;42mtotal of {c1} bill of 1\033[m :bird:', use_aliases='True'))
+line()
+print('Come Again To The KW BANKS ! Have a Great Day')
+
+input()
